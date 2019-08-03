2eed8f5b0d6715f6beb28e3f7008b80632828acc
Ricardo Sparapan <ricardo.jrsparapan@gmail.com>
2019-08-03 17:19:35 -0300

    send email, Jobs with queue

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,10 +1,12 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { subHours, startOfHour, parseISO, isBefore, format } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentController {
   async index(req, res) {
@@ -113,6 +115,44 @@ class AppointmentController {
       content: `Novo agendamento de ${name} para ${dateFormated}`,
       user: provider_id,
     });
+
+    return res.json(appointment);
+  }
+
+  async destroy(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    if (appointment.user_id !== req.userId) {
+      return res.status('401').json({
+        error: "You don't have permission to cancel this appointment.",
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status('401').json({
+        error: 'You can only cancel appointment 2 hours in advence',
+      });
+    }
+
+    appointment.canceled_at = new Date();
+    await appointment.save();
+
+    await Queue.add(CancellationMail.key, { appointment });
 
     return res.json(appointment);
   }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'e612e5a2915887',
+    pass: '3c7af113816526',
+  },
+  default: {
+    from: 'Ricardo Sparapan <ricardo.jrsparapan@gmail.com>',
+  },
+};
